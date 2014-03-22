f7f1adf9e8f9ed174eb5784969a935e5c4f6c8d0
Advantix <info@advantixtech.com.au>
2014-03-22 17:17:07 +0530

    issue fixes
    issue fixes

--- js/dinein.js
+++ js/dinein.js
@@ -32,7 +32,7 @@ jQuery(document).ready(function () {
 	if(bookingDetailsArr!=null) {
 		var bookVal = JSON.parse(bookingDetailsArr);
 		$('#booking_date').val(bookVal.booking_date);//alert(bookVal.seating);
-		$('#comments').val(bookVal.comments);//alert(bookVal.seating);
+		$('#comments').val(bookVal.comments=="" ? 'Additional Requests' : bookVal.comments);//alert(bookVal.seating);
 		$('#num_guest_online').val(bookVal.num_gust_online);
 		$('#num_guest_online-button span').html(bookVal.num_gust_online);
 		if(bookVal.seating=='bkfst') { sittInd=0;} else if(bookVal.seating=='lunch') { sittInd=1;} else if(bookVal.seating=='dinner') { sittInd=2;}
@@ -41,10 +41,11 @@ jQuery(document).ready(function () {
 		loadtimediv('');
 		setTimeout(function() {
 			 var labelidDef=bookVal.time_val;//alert(labelidDef);
-			$("#"+labelidDef+" a").css({"background":'#BFC4C4'});
+			//$("#"+labelidDef+" a").css({"background":'#BFC4C4'});
 			if(labelidDef!=0) {
 				$("#time_validate").val(labelidDef);
-				$("#displayTimeId").html(labelidDef.replace('_',':')+'<span>Arrival Time</span>');
+				$("#timediv").html(labelidDef);
+				$("#timediv-button span").html(labelidDef.replace('_',':'));
 			}
 		}, 700);
 		
@@ -57,18 +58,19 @@ jQuery(document).ready(function () {
 		currDate = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
 		$('#booking_date').val(currDate);//alert(bookVal.num_gust_online);
 	}
+	dateChange('');
 });
 
 function getNumGuest() {
 	resDataMinGuest=resData.min_gust_per_online_bk;
 	resDataMaxGuest=resData.max_gust_per_online_bk;
-	numGuestHtml='<select id="num_guest_online" name="num_guest_online" data-shadow="false">';
+	numGuestHtml='';
 	for(i=resDataMinGuest;i<=resDataMaxGuest;i++){	
 		
 		numGuestHtml+='<option value='+i+'>'+i+'</option>';
 	}		
-	numGuestHtml+='</select><br><span>#Guest</span>';
-	$('#num_guest_online_a').html(numGuestHtml);	
+	//numGuestHtml+='';
+	$('#num_guest_online').html(numGuestHtml);	
 	
 }
 function getSittingName() {	
@@ -113,7 +115,9 @@ function getSittingName() {
 	function loadtimediv(onCh){
 		if(onCh=='drop') {
 			$('#time_validate').val('');
-			$('#displayTimeId').html('Select Arrival Time<span>Arrival Time</span>');
+			
+			$('#timediv').html('<option value="SA">Select Arrival Time</option>');
+			$("#timediv-button span").html('Select Arrival Time');
 		}
 		
 		numGuestSel=$( "#num_guest_online option:selected" ).val();//alert(numGuestSel);
@@ -129,9 +133,6 @@ function getSittingName() {
 				seating=seatTxtBoxVal;
 			}
 			//alert(d+":"+dtstring+":"+seating);
-			
-			$('#timediv').html("<div id='popup'><div id='center'><img src='data/loader.gif' style='color: #FFFFFF;' alt='Loading...' align='absmiddle'></div></div>");
-			
 			//alert(serviceURL+'timedetails/'+d+'/'+n+'/'+restId+'/'+store_id+'/'+seating);
 			
 			$.getJSON(serviceURL+'timedetails/'+d+'/'+restId+'/'+store_id+'/'+seating, function(data) {	
@@ -144,7 +145,7 @@ function getSittingName() {
 					//alert(bkfst);
 					
 					bkfstStrCnt=bkfstStr.length;
-					timHtml='<a href="#" data-rel="back" class="ui-btn ui-corner-all ui-shadow ui-btn-a ui-icon-delete ui-btn-icon-notext ui-btn-right">Close</a><div role="main" class="ui-content clearfix"><h3 class="ui-title">Change your time</h3>';
+					timHtml='';
 					if(bkfstStr.close_rest!==1) {
 						
 						if(bkfstStrCnt > 0) {
@@ -154,7 +155,7 @@ function getSittingName() {
 								//alert(index+":"+bkfst);	
 								if(i==0) { pos='a';} else if(i==1){pos='b';} else if (i==2) { pos='c';}
 								i++;
-								timHtml+= '<div class="time_wrap" id="'+bkfst.replace(':','_')+'"><a href="#" class="ui-btn ui-shadow ui-corner-all" onclick=\'changecolor("'+bkfst.replace(':','_')+'")\' data-rel="back" href="#">'+bkfst+'</a></div>';					
+								timHtml+= '<option value="'+bkfst.replace(':','_')+'">'+bkfst+'</option>';					
 									//alert((bkfstStrCnt%3));
 								if(((index+1)%3)==0) {
 									//alert("dfs");
@@ -163,7 +164,7 @@ function getSittingName() {
 								}
 							});
 							
-							timHtml+= '</div>';
+							//timHtml+= '';
 							$('#timediv').html(timHtml);
 							
 							var bookingDetailsArr = window.localStorage.getItem('bookingDetailsArray');	
@@ -185,22 +186,24 @@ function getSittingName() {
 								}								
 							}						
 						} else {							
-							if(seating=='bkfst') { set=0;} else if(seating=='lunch') {set=1;} else if(seating=='dinner') {set=2;}else {set='Select Sitting';}
-							timHtml+='<div class="ValidationErrors">'+sittingArr[set]+' Not Available for Selected Day</b></div>';
+							if(seating=='bkfst') { set=0;} else if(seating=='lunch') {set=1;} else if(seating=='dinner') {set=2;}else {set='Select Arrival Time';}
 							if(sittingArr[set]==null) {
 								sitVal=set;
 							} else {
-								sitVal=sittingArr[set]+' Closed';
+								sitVal=sittingArr[set]+' Not Available for Selected Day';
 							}
-							$("#displayTimeId").html(sitVal+'<span>Arrival Time</span>');							
-							$("#time_validate").val('');
-							timHtml+= '</div>';
+								
+							timHtml+='<option value="NA">'+sitVal+'</option>';
+												
+							$("#time_validate").val('');							
 							$('#timediv').html(timHtml);
+							$("#timediv-button span").html(sitVal);
 						}
 						
 					} else {
-						timHtml+='<b class="ValidationErrors">Restaurant has been closed on selected date.</b></div>';
+						timHtml+='<option value="CLOSED">Restaurant has been closed on selected date.</option>';
 						$('#timediv').html(timHtml);
+						$("#timediv-button span").html('Restaurant has been closed on selected date.');
 					}
 			});	
 		} else {
@@ -212,21 +215,17 @@ function getSittingName() {
 	
 	function changecolor(labelid){
 		//alert(labelid);		
-		$('#timediv div a').css({"background":'#F4F5F5'});
-		$("#"+labelid+" a").css({"background":'#BFC4C4'});
+		//$('#timediv div a').css({"background":'#F4F5F5'});
+		//$("#"+labelid+" a").css({"background":'#BFC4C4'});
 		if(labelid!=0) {
 			$("#time_validate").val(labelid);
-			$("#displayTimeId").html(labelid.replace('_',':')+'<span>Arrival Time</span>');
+			$("#timediv").val(labelid);
+			$("#timediv-button span").html(labelid.replace('_',':'));
 		}
 		
 	}
 	
-$("#displayTimeId").click(function() {
-	if($('#seating').val()=="Select Sitting") {
-		alert("Please select sitting");
-		return false;
-	}
-});
+
 	
 $("#bookingButtonId").click(function() {	
 	
@@ -236,6 +235,7 @@ $("#bookingButtonId").click(function() {
 	if(time!="") {
 		var booking_date = $('#booking_date').val();
 		var commentsVal = $('#comments').val();
+		commentsVal = commentsVal !="Additional Requests" ? commentsVal: "";
 		var num_guest = $('select[name="num_guest_online"]').val();
 			//alert(serviceURL+'timevalidate/'+time+'/'+booking_date+'/'+restId+'/'+num_guest+'/'+store_id);
 			$.getJSON(serviceURL+'timevalidate/'+time+'/'+booking_date+'/'+restId+'/'+num_guest+'/'+store_id, function(data) {
@@ -275,3 +275,65 @@ $("#bookingButtonId").click(function() {
 			$('#pageLoader').hide();
 		}
 });
+
+function dateChange(type) {
+	
+	var booking_date = $('#booking_date').val();
+	
+	var today = new Date(); 
+	var da = today.getDate();
+	var dm = today.getMonth()+1;
+	currDate = today.getFullYear()+'-'+(dm<10 ? '0'+dm: dm)+'-'+(da<10 ? '0'+da: da);
+	
+	//alert(currDate);
+	dArr=booking_date.split('-');
+	if(dArr[0]<2000) {
+		dateGiven=dArr[2]+'-'+(dArr[1]<10 ? '0'+dArr[1]: dArr[1])+'-'+(dArr[0]<10 ? '0'+dArr[0]: dArr[0]);
+		var nex = new Date(dateGiven); //alert(nex);
+		if(type=='Next') {	
+			nex.setDate(nex.getDate() + 1);		
+		} else if(type=='Prev') {	
+			nex.setDate(nex.getDate() - 1);		
+		} 
+		var dmNex = nex.getMonth()+1;		
+		var dyNex = nex.getFullYear();
+		var daNex = nex.getDate();		
+		
+		var daNex = nex.getDate();			
+		dateGiven = dyNex+'-'+(dmNex<10 ? '0'+dmNex: dmNex)+'-'+(daNex<10 ? '0'+daNex: daNex);			
+		dateGivenHidden=daNex+'-'+dmNex+'-'+dyNex;
+		var cmp = fn_DateCompare(dateGiven,currDate);
+	}
+	//alert(dateGivenHidden);
+	//alert(cmp);
+	if(cmp==0 || cmp==1) {
+		var booking_date = $('#booking_date').val(dateGivenHidden);
+		//alert(booking_date);
+		var frmDat = getDateFormat(dateGiven);
+		var sepDay = frmDat.split(" ");
+		$("#dayP").html(sepDay[0]);
+		$("#dateP").html(sepDay[1]+" "+sepDay[2]+" "+sepDay[3]);
+		//alert(sepDay[0]);
+	} else {
+		alert('Please select greater than or equal to current date');
+	}
+}
+
+function fn_DateCompare(DateA, DateB) {
+
+	 // alert(DateA+":"+DateB);
+      var a = new Date(DateA);
+      var b = new Date(DateB);
+
+      var msDateA = Date.UTC(a.getFullYear(), a.getMonth()+1, a.getDate());
+      var msDateB = Date.UTC(b.getFullYear(), b.getMonth()+1, b.getDate());
+
+      if (parseFloat(msDateA) < parseFloat(msDateB))
+        return -1;  // less than
+      else if (parseFloat(msDateA) == parseFloat(msDateB))
+        return 0;  // equal
+      else if (parseFloat(msDateA) > parseFloat(msDateB))
+        return 1;  // greater than
+      else
+        return null;  // error
+  }
--- js/restaurant_details.js
+++ js/restaurant_details.js
@@ -25,58 +25,79 @@ sitVa = resData.sitting;
 sittingArr = sitVa.split('||');
 // Takeaway box
 
-if(sittingStaArr[0]==0) {
+if(store_id!= 'Mw') {
 
-$('#bkfstId').html('<h2>Dinein Hours ('+sittingArr[0]+')</h2>');
+	if(sittingStaArr[0]==0) {
 
-htmlDataTakeAway='<div class="row_div"><label>Mon </label>   <p class="info_right"> '+checkCloseDate(resData.bk_monday_start, resData.bk_monday_end,resData.bk_monday_notallow)+' </p></div>';
-htmlDataTakeAway+='<div class="row_div"><label>Tue </label>   <p class="info_right"> '+checkCloseDate(resData.bk_tuesday_start, resData.bk_tuesday_end,resData.bk_tuesday_notallow)+'</p></div>';
-htmlDataTakeAway+='<div class="row_div"><label>Wed</label>   <p class="info_right"> '+checkCloseDate(resData.bk_wednesday_start, resData.bk_wednesday_end,resData.bk_wednesday_notallow)+'</p></div>';
-htmlDataTakeAway+='<div class="row_div"><label>Thu</label>   <p class="info_right"> '+checkCloseDate(resData.bk_thursday_start, resData.bk_thursday_end,resData.bk_thursday_notallow)+'</p></div>';
-htmlDataTakeAway+='<div class="row_div"><label>Fri</label>   <p class="info_right"> '+checkCloseDate(resData.bk_friday_start, resData.bk_friday_end,resData.bk_friday_notallow)+'</p></div>';
-htmlDataTakeAway+='<div class="row_div"><label>Sat</label>   <p class="info_right"> '+checkCloseDate(resData.bk_saturday_start, resData.bk_saturday_end,resData.bk_saturday_notallow)+'</p></div>';
-htmlDataTakeAway+='<div class="row_div"><label>Sun</label>   <p class="info_right"> '+checkCloseDate(resData.bk_sunday_start, resData.bk_sunday_end,resData.bk_sunday_notallow)+'</p></div>';
-				
-$('#BreakOpeningHoursDiv').html(htmlDataTakeAway);		
+	$('#bkfstId').html('<h2>Dinein Hours ('+sittingArr[0]+')</h2>');
+
+	htmlDataTakeAway='<div class="row_div"><label>Mon </label>   <p class="info_right"> '+checkCloseDate(resData.bk_monday_start, resData.bk_monday_end,resData.bk_monday_notallow)+' </p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Tue </label>   <p class="info_right"> '+checkCloseDate(resData.bk_tuesday_start, resData.bk_tuesday_end,resData.bk_tuesday_notallow)+'</p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Wed</label>   <p class="info_right"> '+checkCloseDate(resData.bk_wednesday_start, resData.bk_wednesday_end,resData.bk_wednesday_notallow)+'</p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Thu</label>   <p class="info_right"> '+checkCloseDate(resData.bk_thursday_start, resData.bk_thursday_end,resData.bk_thursday_notallow)+'</p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Fri</label>   <p class="info_right"> '+checkCloseDate(resData.bk_friday_start, resData.bk_friday_end,resData.bk_friday_notallow)+'</p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Sat</label>   <p class="info_right"> '+checkCloseDate(resData.bk_saturday_start, resData.bk_saturday_end,resData.bk_saturday_notallow)+'</p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Sun</label>   <p class="info_right"> '+checkCloseDate(resData.bk_sunday_start, resData.bk_sunday_end,resData.bk_sunday_notallow)+'</p></div>';
+					
+	$('#BreakOpeningHoursDiv').html(htmlDataTakeAway);		
+	} else {
+		$('#bkfstId').hide();
+		$('#BreakOpeningHoursDiv').hide();
+	}
+
+	if(sittingStaArr[1]==0) {
+	// Delivery box
+	$('#lunchId').html('<h2>Dinein Hours ('+sittingArr[1]+')</h2>');
+	htmlDatadelivery='<div class="row_div"><label>Mon</label>   <p class="info_right"> '+checkCloseDate(resData.lun_monday_start, resData.lun_monday_end,resData.lun_monday_notallow)+' </p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Tue </label>   <p class="info_right"> '+checkCloseDate(resData.lun_tuesday_start, resData.lun_tuesday_end,resData.lun_tuesday_notallow)+'</p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Wed</label>   <p class="info_right"> '+checkCloseDate(resData.lun_wednesday_start, resData.lun_wednesday_end,resData.lun_wednesday_notallow)+'</p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Thu</label>   <p class="info_right"> '+checkCloseDate(resData.lun_thursday_start, resData.lun_thursday_end,resData.lun_thursday_notallow)+'</p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Fri</label>   <p class="info_right"> '+checkCloseDate(resData.lun_friday_start, resData.lun_friday_end,resData.lun_friday_notallow)+'</p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Sat</label>   <p class="info_right"> '+checkCloseDate(resData.lun_saturday_start, resData.lun_saturday_end,resData.lun_saturday_notallow)+'</p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Sun</label>   <p class="info_right"> '+checkCloseDate(resData.lun_sunday_start, resData.lun_sunday_end,resData.lun_sunday_notallow)+'</p></div>';
+					
+	$('#LunchOpeningHoursDiv').html(htmlDatadelivery);	
+	} else {
+		$('#lunchId').hide();
+		$('#LunchOpeningHoursDiv').hide();
+	}
+
+	if(sittingStaArr[2]==0) {
+
+	// Delivery box
+	$('#dineId').html('<h2>Dinein Hours ('+sittingArr[2]+')</h2>');
+	htmlDatadelivery='<div class="row_div"><label>Mon</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_monday_start, resData.dinner_monday_end,resData.dinner_monday_notallow)+' </p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Tue </label>   <p class="info_right"> '+checkCloseDate(resData.dinner_tuesday_start, resData.dinner_tuesday_end,resData.dinner_tuesday_notallow)+'</p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Wed</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_wednesday_start, resData.dinner_wednesday_end,resData.dinner_wednesday_notallow)+'</p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Thu</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_thursday_start, resData.dinner_thursday_end,resData.dinner_thursday_notallow)+'</p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Fri</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_friday_start, resData.dinner_friday_end,resData.dinner_friday_notallow)+'</p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Sat</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_saturday_start, resData.dinner_saturday_end,resData.dinner_saturday_notallow)+'</p></div>';
+	htmlDatadelivery+='<div class="row_div"><label>Sun</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_sunday_start, resData.dinner_sunday_end,resData.dinner_sunday_notallow)+'</p></div>';
+					
+	$('#DinnerOpeningHoursDiv').html(htmlDatadelivery);	
+
+	} else {
+		$('#dineId').hide();
+		$('#DinnerOpeningHoursDiv').hide();
+	}
 } else {
-	$('#bkfstId').hide();
-	$('#BreakOpeningHoursDiv').hide();
-}
+	if(sittingStaArr[0]==0) {
 
-if(sittingStaArr[1]==0) {
-// Delivery box
-$('#lunchId').html('<h2>Dinein Hours ('+sittingArr[1]+')</h2>');
-htmlDatadelivery='<div class="row_div"><label>Mon</label>   <p class="info_right"> '+checkCloseDate(resData.lun_monday_start, resData.lun_monday_end,resData.lun_monday_notallow)+' </p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Tue </label>   <p class="info_right"> '+checkCloseDate(resData.lun_tuesday_start, resData.lun_tuesday_end,resData.lun_tuesday_notallow)+'</p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Wed</label>   <p class="info_right"> '+checkCloseDate(resData.lun_wednesday_start, resData.lun_wednesday_end,resData.lun_wednesday_notallow)+'</p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Thu</label>   <p class="info_right"> '+checkCloseDate(resData.lun_thursday_start, resData.lun_thursday_end,resData.lun_thursday_notallow)+'</p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Fri</label>   <p class="info_right"> '+checkCloseDate(resData.lun_friday_start, resData.lun_friday_end,resData.lun_friday_notallow)+'</p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Sat</label>   <p class="info_right"> '+checkCloseDate(resData.lun_saturday_start, resData.lun_saturday_end,resData.lun_saturday_notallow)+'</p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Sun</label>   <p class="info_right"> '+checkCloseDate(resData.lun_sunday_start, resData.lun_sunday_end,resData.lun_sunday_notallow)+'</p></div>';
-				
-$('#LunchOpeningHoursDiv').html(htmlDatadelivery);	
-} else {
-	$('#lunchId').hide();
-	$('#LunchOpeningHoursDiv').hide();
-}
+	$('#bkfstId').html('<h2>Opening Hours</h2>');
 
-if(sittingStaArr[2]==0) {
-
-// Delivery box
-$('#dineId').html('<h2>Dinein Hours ('+sittingArr[2]+')</h2>');
-htmlDatadelivery='<div class="row_div"><label>Mon</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_monday_start, resData.dinner_monday_end,resData.dinner_monday_notallow)+' </p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Tue </label>   <p class="info_right"> '+checkCloseDate(resData.dinner_tuesday_start, resData.dinner_tuesday_end,resData.dinner_tuesday_notallow)+'</p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Wed</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_wednesday_start, resData.dinner_wednesday_end,resData.dinner_wednesday_notallow)+'</p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Thu</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_thursday_start, resData.dinner_thursday_end,resData.dinner_thursday_notallow)+'</p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Fri</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_friday_start, resData.dinner_friday_end,resData.dinner_friday_notallow)+'</p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Sat</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_saturday_start, resData.dinner_saturday_end,resData.dinner_saturday_notallow)+'</p></div>';
-htmlDatadelivery+='<div class="row_div"><label>Sun</label>   <p class="info_right"> '+checkCloseDate(resData.dinner_sunday_start, resData.dinner_sunday_end,resData.dinner_sunday_notallow)+'</p></div>';
-				
-$('#DinnerOpeningHoursDiv').html(htmlDatadelivery);	
-
-} else {
-	$('#dineId').hide();
-	$('#DinnerOpeningHoursDiv').hide();
+	htmlDataTakeAway='<div class="row_div"><label>Mon </label>   <p class="info_right"> Closed </p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Tue </label>   <p class="info_right"> 5:30 - 9:30 </p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Wed</label>   <p class="info_right"> 5:30 - 9:30</p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Thu</label>   <p class="info_right"> 11:30 - 2:30 / 5:30 - 9:30 </p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Fri</label>   <p class="info_right"> 11:30 - 2:30 / 5:30 - 10:00</p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Sat</label>   <p class="info_right"> 11:30 - 2:30 / 5:30 - 10:00</p></div>';
+	htmlDataTakeAway+='<div class="row_div"><label>Sun</label>   <p class="info_right"> 11:30 - 2:30 / 5:30 - 9:30</p></div>';
+					
+	$('#BreakOpeningHoursDiv').html(htmlDataTakeAway);		
+	} else {
+		$('#bkfstId').hide();
+		$('#BreakOpeningHoursDiv').hide();
+	}
 }
 headerHtml('');
 
